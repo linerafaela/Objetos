4e58615bae03334f81ed99879d8161fb94fa5f5f
linerafaela <128156711+linerafaela@users.noreply.github.com>
2024-06-03 14:24:54 -0300

    Add files via upload

---
+++ Objetos-main/Objetos/aula01/objetos.js
@@ -0,0 +1,8 @@
+const iformaçõesPessoa = ['nome','Line','idade',17,'CPF','11112223333'];
+
+const objetoPessoa = {
+    nome: 'Line',
+    idade: 17,
+    CPF: '11112223333',
+    email: 'aline.rafaela.santos@escola.pr.gov.br'
+}
