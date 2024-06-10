5f1b086de663d3a60a93fab91386278c02d8c5b8
linerafaela <128156711+linerafaela@users.noreply.github.com>
2024-06-10 09:04:21 -0300

    Add files via upload

---
+++ Objetos-main/Objetos/Aula01/acesso_ponto.js
@@ -0,0 +1,8 @@
+const objetoPessoa = {
+    nome: 'Line',
+    idade: 17,
+    CPF: '11112223333',
+    email: 'aline.rafaela.santos@escola.pr.gov.br'
+}
+//Acessando com os pontos
+console.log(`O nome do cliente é ${objetoPessoa.nome} e sua idade ${objetoPessoa.idade}`)
---
+++ Objetos-main/Objetos/Aula01/alterar.js
@@ -0,0 +1,10 @@
+const pessoa = {
+    nome: 'Roberta',
+    profissao: 'Modelo'
+};
+
+console.log(pessoa.telefone)
+
+pessoa.telefone = '43998429733'
+
+console.log(pessoa.telefone);
